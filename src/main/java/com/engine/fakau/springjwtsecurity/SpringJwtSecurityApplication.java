a99ab9a7d58404387ba57e6c4fac4b7ee5d78016
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-18 22:03:41 -0400

    init

---
+++ src/main/java/com/engine/fakau/springjwtsecurity/SpringJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springjwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engine/fakau/springjwtsecurity/SpringJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springjwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
